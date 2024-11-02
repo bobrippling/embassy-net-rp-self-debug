098189ef997f2bd9791b5b7397946161e2486e0a
Rob Pilling <robpilling@gmail.com>
2024-11-02 13:07:05 +0000

    Revert "flash algo: remove any loading from memory"
    This reverts commit e26736ec9fd71ef18befa71c7bc0156590769465.
    
    No change in behaviour / erase bug is elsewhere

--- src/flash_standalone.rs
+++ src/flash_standalone.rs
@@ -3,45 +3,28 @@
 
 type IpcFn = extern "C" fn(usize, usize, usize) -> !;
 
-const P: usize = 0x20000000;
-//const THUNK: *const [IpcFn; 4] = P as _;
+const THUNK: *const [IpcFn; 4] = 0x20000000 as _;
 
 #[link_section = ".text"]
 #[no_mangle]
 fn init(address: usize, clock_or_zero: usize, op: usize/*Operation*/) -> ! {
-    unsafe {
-        let p = core::hint::black_box(P) + 4 * 0;
-        let p: IpcFn = core::mem::transmute(p);
-        p(address, clock_or_zero, op)
-    }
+    unsafe { (*THUNK)[0](address, clock_or_zero, op) }
 }
 
 #[link_section = ".text"]
 #[no_mangle]
 fn uninit(op: usize/*Operation*/) -> ! {
-    unsafe {
-        let p = core::hint::black_box(P) + 4 * 1;
-        let p: IpcFn = core::mem::transmute(p);
-        p(op, 0, 0)
-    }
+    unsafe { (*THUNK)[1](op, 0, 0) }
 }
 
 #[link_section = ".text"]
 #[no_mangle]
 fn program_page(address: usize, byte_len: usize, buffer: *const u8) -> ! {
-    unsafe {
-        let p = core::hint::black_box(P) + 4 * 2;
-        let p: IpcFn = core::mem::transmute(p);
-        p(address, byte_len, buffer as _)
-    }
+    unsafe { (*THUNK)[2](address, byte_len, buffer as _) }
 }
 
 #[link_section = ".text"]
 #[no_mangle]
 fn erase_sector(address: usize) -> ! {
-    unsafe {
-        let p = core::hint::black_box(P) + 4 * 3;
-        let p: IpcFn = core::mem::transmute(p);
-        p(address, 0, 0)
-    }
+    unsafe { (*THUNK)[3](address, 0, 0) }
 }
